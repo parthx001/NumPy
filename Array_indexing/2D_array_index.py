693c38eefe527d70785e67723a9dea17a38cc7f6
Parth <parthagarai001@gmail.com>
2026-03-14 13:16:20 +0530

    adding next topics

---
+++ Array_indexing/2D_array_index.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([
+    [3,6,5],
+    [5,6,4]
+])
+print(arr[1,2])
+print(arr[1]) # printing entire row
+print(arr[:,2]) # printing entire column
---
+++ Array_indexing/3D_index.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr=np.array([
+    [[2,4,3],[5,6,7]],
+    [[9,8,7],[6,5,3]]
+])
+print(arr[1,0,2])
